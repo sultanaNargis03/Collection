f35e25490aad139dee6026f6fb8b3a4ff6bf7e7d
sultanaNargis03 <nargissultana969@gmail.com>
2024-04-09 19:13:31 +0530

    "HashSet"

---
+++ src/practice/LaunchHashSet.java
@@ -0,0 +1,23 @@
+package practice;
+
+import java.util.HashSet;
+
+public class LaunchHashSet {
+
+	public static void main(String[] args) 
+	{
+		
+		HashSet hs=new HashSet();
+		hs.add(100);
+		hs.add("Java");
+		hs.add(30);
+		hs.add(40);
+		hs.add(10);
+//		hs.add(10);
+		hs.add("");
+		System.out.println(hs);
+		
+		
+	}
+
+}
